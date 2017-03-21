661f099740335fa7b2b65e33b36b9389e1d9e33f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-03-21 08:58:58 -0300

    fix ICLASS support in replace_class_by_address

--- lib/memdump/replace_class_address_by_name.rb
+++ lib/memdump/replace_class_address_by_name.rb
@@ -2,9 +2,28 @@ module MemDump
     # Replace the address in the 'class' attribute by the class name
     def self.replace_class_address_by_name(dump, add_reference_to_class: false)
         class_names = Hash.new
+        iclasses = Hash.new
         dump.each_record do |row|
             if row['type'] == 'CLASS' || row['type'] == 'MODULE'
                 class_names[row['address']] = row['name']
+            elsif row['type'] == 'ICLASS'
+                iclasses[row['address']] = row
+            end
+        end
+
+        iclass_size = 0
+        while !iclasses.empty? && (iclass_size != iclasses.size)
+            iclass_size = iclasses.size
+            iclasses.delete_if do |_, r|
+                if (klass = r['class']) && (class_name = class_names[klass])
+                    class_names[r['address']] = "I(#{class_name})"
+                    r['class'] = class_name
+                    r['class_address'] = klass
+                    if add_reference_to_class
+                        (r['references'] ||= Array.new) << klass
+                    end
+                    true
+                end
             end
         end
 
@@ -15,9 +34,6 @@ module MemDump
                 if add_reference_to_class
                     (r['references'] ||= Array.new) << klass
                 end
-            end
-            if r['type'] == 'ICLASS'
-                r['class'] = "I(#{r['class']})"
             end
             r
         end
